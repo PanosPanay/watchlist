90822e68fcfbdcafd65efd5d6418185ec8469cd0
PanosPanay <edwinwangbj@outlook.com>
2019-12-21 15:45:21 +0800

    Create and delete item by form

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy     # 导入 Flask-SQLAlchemy 扩展类
 import click
 
@@ -12,6 +12,7 @@ else:       # 否则使用4个斜线
     prefix = 'sqlite:////'
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dev'    # flash消息签名所需的密钥，等同于app.secret_key = 'dev'
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db') # 数据库连接地址
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False    # 关闭对模型修改的监控
 
@@ -71,12 +72,12 @@ def forge():
     ]
 
     user = User(name=name)
-    db.session.add(user)
+    db.session.add(user)                        # 添加到数据库会话
     for m in movies:
         movie = Movie(title=m['title'], year=m['year'])
-        db.session.add(movie)
+        db.session.add(movie)                   # 添加到数据库会话
 
-    db.session.commit()
+    db.session.commit()                         # 提交数据库会话
     click.echo('Done.')                         # 输出提示信息
 
 
@@ -87,14 +88,62 @@ def inject_user():  # 函数名可以随意修改
     return dict(user=user)  # 需要返回字典，等同于return {'user': user}
     # 这个函数返回的变量（以字典键值对的形式）将会统一注入到每一个模板的上下文环境中，因此可以直接在模板中使用
 
-@app.route('/')
-def index():
-    movies = Movie.query.all()                      # 读取所有电影记录
-    return render_template('index.html', movies=movies)
-
 @app.errorhandler(404)      # 传入要处理的错误代码
 def page_not_found(e):      # 接受异常对象作为参数
     return render_template('404.html'), 404  # 返回模板和状态码
+
+@app.route('/', methods=['GET', 'POST'])        # methods表示同时接受GET和POST请求，否则默认只接受GET请求
+def index():
+    """主页"""
+    if request.method == 'POST':                # 判断是否是POST请求
+        # 获取表单数据
+        title = request.form.get('title')       # request.form是一个特殊的字典
+        year = request.form.get('year')
+        # 验证数据
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            # flash() 函数用来在视图函数里向模板传递提示消息，get_flashed_messages() 函数则用来在模板中获取提示消息
+            flash('Invalid input.') # 显示错误提示
+            return redirect(url_for('index'))   # 重定向回主页
+        # 保存表单数据到数据库
+        movie = Movie(title=title, year=year)   # 创建记录
+        db.session.add(movie)                   # 添加到数据库会话
+        db.session.commit()                     # 提交数据库会话
+        flash('Item created.')                  # 显示成功创建的提示
+        return redirect(url_for('index'))       # 重定向回主页
+
+    movies = Movie.query.all()                  # 读取所有电影记录
+    return render_template('index.html', movies=movies)
+
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    """编辑电影条目页面"""
+    movie = Movie.query.get_or_404(movie_id)    # 获取待更新的电影记录
+
+    if request.method == 'POST':                # 处理编辑表单的提交请求
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))     # 重定向回对应的编辑页面
+
+        movie.title = title                     # 更新标题
+        movie.year = year                       # 更新年份
+        db.session.commit()                     # 提交数据库会话
+        flash('Item updated.')
+        return redirect(url_for('index'))       # 重定向回主页
+
+    return render_template('edit.html', movie=movie)    # 传入被编辑的电影记录
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])    # 限定只接受POST请求
+def delete(movie_id):
+    """只相应删除电影条目按钮的操作，无单独页面"""
+    movie = Movie.query.get_or_404(movie_id)    # 获取电影记录
+    db.session.delete(movie)                    # 删除对应的记录
+    db.session.commit()                         # 提交数据库会话
+    flash('Item deleted.')
+    return redirect(url_for('index'))           # 重定向回主页
+
 
 @app.route('/user/<name>')
 def user_page(name):
